01a3da9ae819668de6ae99dc54d9a65d2664cc55
mspoonam <mspoonam.learning@gmail.com>
2022-11-06 07:50:35 -0500

    added animation for man walking

--- SampleMusicFile/SampleMusicFile/ContentView.swift
+++ SampleMusicFile/SampleMusicFile/ContentView.swift
@@ -13,29 +13,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            VideoPlayer(player: AVPlayer(url:  vm.getVideoURL()!))
-                .frame(height: 400)
-
-            Button(action: {
-                print("Button action")
-                vm.playSound()
-            }) {
-                Text("Button label")
-            }
-            .frame(width: 100, height: 40)
-            .border(.gray)
-            
-            Button(action: {
-                print("Button action")
-                print("video file path is \(vm.getVideoURL())")
-            }) {
-                Text("Video File")
-            }
-            .frame(width: 100, height: 40)
-            .border(.gray)
-           
+            BicycleView()
         }
-     
     }
 }
 
@@ -43,5 +22,83 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(vm: .init())
         
+    }
+}
+
+struct BicycleView: View {
+    @State private var rightPedaling = false
+    @State private var jumping = false
+    @State private var leftPedaling = false
+    
+   
+    @State var tyre1  = false
+    @State var tyre2 = false
+    
+    var body: some View {
+        ZStack{
+        VStack{
+        
+        ZStack{
+            
+          
+            
+            VStack{
+                //Bycycle Animation
+                HStack{
+                    ZStack {
+                        ZStack {
+                            
+                            Image("leg_l")  // Left Leg
+                                .animation(Animation.interpolatingSpring(stiffness: 200, damping: 50).speed(3).repeatForever(autoreverses: true))
+                                .rotationEffect(.degrees(leftPedaling ? -5 : 15), anchor: .topLeading)
+                                .offset(x: -30, y: leftPedaling ? -50 : -65)
+                                .onAppear() {
+                                self.leftPedaling.toggle()
+                                }
+                            Image("face")  // Biker
+                                .offset(y: -95)
+                            
+                            Image("leg_r") // Right leg
+                                .animation(Animation.interpolatingSpring(stiffness: 200, damping: 50).speed(3).repeatForever(autoreverses: true))
+                                .rotationEffect(.degrees(rightPedaling ? 15 : -15), anchor: .topLeading)
+                                .offset(x: -40, y: rightPedaling ? -45 : -60)
+                                .onAppear() {
+                                    self.rightPedaling.toggle()
+                                    }
+                        }
+                        .animation(Animation.interpolatingSpring(stiffness: 200, damping: 50).speed(3).repeatForever(autoreverses: true))
+                         .offset(y: jumping ? -1 : 1)
+                         .onAppear() {
+                            self.jumping.toggle()
+                        }
+                    }
+                    HStack{
+                    Spacer()
+                        .frame(width: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    VStack{
+                    Text("100")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                    
+                    Text("METERS")
+                        .font(.footnote)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                    }
+                    }
+                    
+                }
+            }
+        }
+            
+            Rectangle()
+                .frame(width: 600, height: 5, alignment: .center)
+                .foregroundColor(Color("Color"))
+                .cornerRadius(25)
+         }
+        
+        }
+       
     }
 }
